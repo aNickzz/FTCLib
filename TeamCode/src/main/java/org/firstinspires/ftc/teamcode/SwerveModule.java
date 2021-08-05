187609a242dfbaf2d0021694ffa750fbc5f0e879
baileyWooldridge <bailey.wooldridge@roboroos.org.au>
2021-08-05 14:42:25 +0930

    Optimized swerve module
    Added a function to optimize the rotation of the swerve module.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveModule.java
@@ -1,11 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.geometry.Rotation2d;
-import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.arcrobotics.ftclib.hardware.motors.CRServo;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.SwerveModuleState;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+
 
 public class SwerveModule {
 
@@ -23,18 +22,46 @@ public class SwerveModule {
         runModuleState();
     }
 
-    private double getAzimuthAngle() {
-        double val = 0.0;
-        return val;
+    private Rotation2d getAzimuthAngle() {
+        return Rotation2d.fromDegrees(0);
     }
 
     private void runModuleState() {
+        // set the drive motor to drive
         driveMotor.set(moduleState.speedMetersPerSecond);
 
-        if(moduleState.angle.getDegrees()+1.5 < getAzimuthAngle()) {
+        if(moduleState.angle.getDegrees()+1.5 < getAzimuthAngle().getDegrees()) {
+            // rotate the module ccw
             azimuthServo.set(-0.8);
-        } else if(moduleState.angle.getDegrees()-1.5 > getAzimuthAngle()) {
+        } else if(moduleState.angle.getDegrees()-1.5 > getAzimuthAngle().getDegrees()) {
+            // rotate the module cw
             azimuthServo.set(0.8);
         }
     }
+
+    private void runModuleStateOptimised() {
+        double speed = moduleState.speedMetersPerSecond;
+        double targetAngle = moduleState.angle.getDegrees();
+
+        // find the optimised angle to rotate the module
+        if(
+                Math.abs(moduleState.angle.getDegrees() - getAzimuthAngle().getDegrees()) > 90 &&
+                Math.abs(moduleState.angle.getDegrees() - getAzimuthAngle().getDegrees()) < 270
+        ) {
+            targetAngle = ((int)targetAngle + 180) % 360;
+            speed = -speed;
+        }
+
+        // rotate the module
+        if(targetAngle+1.5 < getAzimuthAngle().getDegrees()) {
+            // rotate the module ccw
+            azimuthServo.set(-0.8);
+        } else if(targetAngle-1.5 > getAzimuthAngle().getDegrees()) {
+            // rotate the module cw
+            azimuthServo.set(0.8);
+        }
+
+        driveMotor.set(speed);
+    }
+
 }
